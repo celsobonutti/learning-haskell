14bc1dacc742f6df1ee3740d616a2ac651220688
Celso Bonutti Filho <celso.bonuttif@gmail.com>
2020-12-20 20:19:27 -0300

    adds zipping

---
+++ Zipping.hs
@@ -0,0 +1,11 @@
+module Zipping where
+
+myZip :: [a] -> [b] -> [(a, b)]
+myZip [] _ = []
+myZip _ [] = []
+myZip (x : xs) (y : ys) = (x, y) : myZip xs ys
+
+myZipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+myZipWith _ [] _ = []
+myZipWith _ _ [] = []
+myZipWith f (x : xs) (y : ys) = f x y : myZipWith f xs ys
