e2de59d7e44a6cc9c050d2cd3987430df413a8be
Celso Bonutti Filho <celso.bonuttif@gmail.com>
2021-02-11 22:44:29 -0300

    adds Capitalize

---
+++ Capitalize.hs
@@ -0,0 +1,19 @@
+module Capitalize where
+
+import Data.Char (toUpper)
+
+capitalizeWord :: String -> String
+capitalizeWord "" = ""
+capitalizeWord (x : xs) = toUpper x : xs
+
+capitalizeParagraph :: String -> String
+capitalizeParagraph paragraph = unwords . snd . foldl fn (True, []) $ wrds
+ where
+  wrds = words paragraph
+  fn
+    (shouldCapitalize, words)
+    currentWord =
+      ( last currentWord == '.'
+      , words
+          ++ [(if shouldCapitalize then capitalizeWord else id) currentWord]
+      )
